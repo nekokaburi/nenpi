0f41b3d3023a7e099b8a08d6f94937aeaf4674eb
智之 <@192.168.1.6>
2016-07-09 10:29:00 +0900

    表示用jsp作成

--- nenpi/src/servlet/Main.java
+++ nenpi/src/servlet/Main.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,8 +28,10 @@ public class Main extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher dispatch = request.getRequestDispatcher("WEB-INF/jsp/main.jsp");
+
+		  dispatch.forward(request, response);
+
 	}
 
 	/**
